586149f6aa10989a2200bbe1c2de0fcc52e6dbcf
bachchupatra67-dotcom <bachchupatra67@gmail.com>
2026-04-12 10:32:52 +0530

    Update gemini.js

--- netlify/functions/gemini.js
+++ netlify/functions/gemini.js
@@ -16,7 +16,7 @@ exports.handler = async function(event, context) {
             console.log("2. The API key was successfully found in the Vault.");
         }
 
-        const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
+        const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${API_KEY}`;
 
         const response = await fetch(url, {
             method: "POST",
